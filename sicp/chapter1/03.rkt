54722b069bfc5320e026faaf1fdf1f03c605b32b
Ilya Leykin <imleykin@gmail.com>
2019-07-11 13:14:10 +0500

    fix wrong solution

--- sicp/chapter1/03.rkt
+++ sicp/chapter1/03.rkt
@@ -2,16 +2,16 @@
 
 (#%require rackunit)
 
-(define (sum-of-largest-squares a b c)
-  (cond ((and (>= a b) (>= b c)) (sum-of-squares a b))
-        ((and (>= a b) (>= c b)) (sum-of-squares a c))
-        (else (sum-of-squares c b))))
+(define (sum-of-squares-of-top-two a b c)
+  (cond ((and (< a b) (< a c)) (sum-of-squares b c))
+        ((and (< b a) (< b c)) (sum-of-squares a c))
+        (else (sum-of-squares a b))))
 
 (define (sum-of-squares a b)
   (+ (square a) (square b)))
 
 (define (square a) (* a a))
 
-(check-equal? (sum-of-largest-squares 1 2 3) 13)
-(check-equal? (sum-of-largest-squares 4 5 2) 29)
-(check-equal? (sum-of-largest-squares 7 10 3) 109)
+(check-equal? (sum-of-squares-of-top-two 1 2 3) 13)
+(check-equal? (sum-of-squares-of-top-two 3 4 2) 25)
+(check-equal? (sum-of-squares-of-top-two 5 3 4) 41)
